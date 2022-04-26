eb1d2cab4d64bf2432f517a850895e8612ec260d
HeliJuS <103338394+HeliJuS@users.noreply.github.com>
2022-04-26 20:24:01 -0300

    Create LACO-1.cpp
    Crie um programa que conte de 1 até 100 ,exibindo cada número.

---
+++ LACO-1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	for (int lapis = 1; lapis <= 100; lapis++) // for (variavel = valor da varial; condição; incrementador || decrementador)
+
+		printf("%d\t", lapis);
+
+		system("pause");
+	    return (0);
+}
